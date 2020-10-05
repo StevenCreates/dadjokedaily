c6720ebe74765248fd760d0455f35b749733d1ae
Steven Hofheins <stevenisinsane@gmail.com>
2020-10-04 22:01:33 -0600

    finished styling for favorites

--- client/src/components/Favorites.js
+++ client/src/components/Favorites.js
@@ -9,8 +9,36 @@ export default function Favorites() {
   const FavoriteContainer = styled.div`
     margin-left: 7rem;
     margin-right: 7rem;
+    /* border-radius: 8px; */
+    height: 30vh;
+    max-height: 30vh;
+    overflow-y: scroll;
+  `;
+
+  const CommunityDiv = styled.div`
+    width: full;
+    padding-top: 0.5rem;
+    padding-bottom: 0.5rem;
+    background: #dfbb4f;
+    font-weight: 700;
+    margin-bottom: 0.25rem;
+    color: #f4f3f3;
     border-radius: 8px;
-    border: sold #7f8994;
+    padding-left: 0.25rem;
+  `;
+
+  const CommunitySpan = styled.div`
+    color: #f4f3f3;
+    padding-top: 0.5rem;
+    padding-bottom: 0.5rem;
+    text-align: center;
+    font-size: 24px;
+    font-weight: 900;
+    margin-top: 3rem;
+    border-bottom: solid #7f8994 3px;
+    margin-left: 7rem;
+    margin-right: 7rem;
+    margin-bottom: 1rem;
   `;
 
   React.useEffect(() => {
@@ -24,11 +52,17 @@ export default function Favorites() {
   }, [db]);
 
   return (
-    <FavoriteContainer>
-      <span>Community Favorites</span>
-      {favorites.map((joke) => (
-        <div>{joke.favorite ? joke.favorite.joke : "waiting"}</div>
-      ))}
-    </FavoriteContainer>
+    <>
+      <CommunitySpan>
+        <span>Community Favorites</span>
+      </CommunitySpan>
+      <FavoriteContainer>
+        {favorites.map((joke) => (
+          <CommunityDiv>
+            {joke.favorite ? joke.favorite.joke : "waiting"}
+          </CommunityDiv>
+        ))}
+      </FavoriteContainer>
+    </>
   );
 }
